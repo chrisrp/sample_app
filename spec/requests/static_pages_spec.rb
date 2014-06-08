fb315b666a139a07c5ea5e22452394dcacae7f60
chrisrp <chrisrp@inventati.org>
2014-06-08 12:32:29 -0300

    adding full_title helper

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -1,41 +1,35 @@
 require 'spec_helper'
 
 describe "Static pages" do
+  let(:title) { 'Ruby on Rails Tutorial Sample App' }
 
   describe "Home page" do
-    it "should have the content 'Sample App'" do
-      visit '/static_pages/home'
-      expect(page).to have_content('Sample App')
-    end
+    before(:each) { visit '/static_pages/home' }
 
-    it "should have the right title" do
-      visit '/static_pages/home'
-      expect(page).to have_title("Ruby on Rails Tutorial Sample App | Home")
-    end
+    it { expect(page).to have_content('Sample App')}
+    it { expect(page).to have_title("#{title}") }
+    it { expect(page).not_to have_title(" | Home") }
   end
 
   describe "Help page" do
-    it "should have the content 'Help'" do
-      visit '/static_pages/help'
-      expect(page).to have_content('Help')
-    end
+    before(:each) { visit '/static_pages/help' }
 
-    it "should have the right title" do
-      visit '/static_pages/help'
-      expect(page).to have_title("Ruby on Rails Tutorial Sample App | Help")
-    end
+    it { expect(page).to have_content('Help') }
+    it { expect(page).to have_title("#{title} | Help") }
   end
 
   describe "About page" do
-    it "should have the content 'About Us'" do
-      visit '/static_pages/about'
-      expect(page).to have_content('About Us')
-    end
+    before(:each) { visit '/static_pages/about' }
 
-    it "should have the right title" do
-      visit '/static_pages/about'
-      expect(page).to have_title("Ruby on Rails Tutorial Sample App | About Us")
-    end
+    it { expect(page).to have_content('About Us') }
+    it { expect(page).to have_title("#{title}  | About Us") }
+  end
+
+  describe "Contact" do
+    before(:each) { visit '/static_pages/contact' }
+
+    it { expect(page).to have_title("#{title}  | Contact") }
+    it { expect(page).to have_content('Contact') }
   end
 
 end
