4c436441c1e4ac1ff8f0e5e6f5f8b9ee894dee95
chrisrp <chrisrp@inventati.org>
2014-06-27 21:52:13 -0300

    excercises

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -1,36 +1,44 @@
 require 'spec_helper'
 
 describe "Static pages" do
-  let(:title) { 'Ruby on Rails Tutorial Sample App' }
+  before { visit root_path }
 
   subject { page }
 
-  describe "Home page" do
-    before { visit root_path }
+  shared_examples_for 'all static pages' do
+    it { should have_selector('h1', text: heading) }
+    it { should have_title(full_title(page_title)) }
+  end
 
-    it { should have_content('Sample App')}
-    it { should have_title("#{title}") }
+  describe "Home page" do
+    let(:heading) { 'Sample App'}
+    let(:page_title) { '' }
+
+    it_should_behave_like 'all static pages'
     it { should_not have_title(" | Home") }
   end
 
   describe "Help page" do
-    before { visit help_path }
+    before { click_link 'Help' }
+    let(:heading)    { 'Help'}
+    let(:page_title) { 'Help' }
 
-    it { should have_content('Help') }
-    it { should have_title("#{title} | Help") }
+    it_should_behave_like 'all static pages'
   end
 
   describe "About page" do
-    before { visit about_path }
+    before { click_link 'About' }
+    let(:heading)    { 'About Us'}
+    let(:page_title) { 'About Us' }
 
-    it { should have_content('About Us') }
-    it { should have_title("#{title}  | About Us") }
+    it_should_behave_like 'all static pages'
   end
 
   describe "Contact" do
-    before { visit contact_path }
+    before { click_link 'Contact' }
+    let(:heading)    { 'Contact'}
+    let(:page_title) { 'Contact' }
 
-    it { should have_title("#{title}  | Contact") }
-    it { should have_content('Contact') }
+    it_should_behave_like 'all static pages'
   end
 end
