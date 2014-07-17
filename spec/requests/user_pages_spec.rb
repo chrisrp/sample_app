0fcbd25d9b60e9bb757dd4c2a455515d9dae7e99
chrisrp <chrisrp@inventati.org>
2014-07-17 11:38:38 -0300

    signup branch

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -9,6 +9,15 @@ describe "User Pages" do
     before { visit signup_path }
 
     it { should have_content('Sign up') }
-    it { should have_title("#{title} | Sign up")}
+    it { should have_title(full_title("Sign up")) }
+  end
+
+  describe 'Profile Page' do
+    let(:user) { FactoryGirl.create(:user) }
+
+    before { visit user_path(user) }
+
+    it { should have_content(user.name) }
+    it { should have_title(user.name) }
   end
 end
